ad56f8e7e63d2e34f509ff78d67de6a27e548c95
Nayara Covas <nayaracovas1@outlook.com>
2023-06-13 23:01:47 +0200

    Create full question and comments page

--- config/route.js
+++ config/route.js
@@ -3,6 +3,7 @@ const userController = require('../controller/userController');
 //const signupController = require('../controller/signupController')
 const auth = require('../middleware/auth')
 
+const fullQuestionController = require('../controller/fullQuestionController')
 const route = express.Router();
 
 route.get('/', userController.homePage);
@@ -15,7 +16,21 @@ route.get('/addQuestion', userController.questionAddition);
 
 route.post('/postQuestion', userController.postQuestion);
 
+//see full question 
 
+route.get('/question/:id', fullQuestionController.getQuestionDetails);
+
+
+// edit question
+route.get('/question/edit/:id', fullQuestionController.editQuestion);
+route.post('/update-question/:id', fullQuestionController.updateQuestion);
+
+//delete question
+route.post('/delete-question/:id', fullQuestionController.deleteQuestion);
+
+//comment a question
+
+route.post('/question/:id/comment',fullQuestionController.addComment);
 
 
 module.exports = route;
--- model/comment.js
+++ model/comment.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
 const commentSchema = new mongoose.Schema({
     userId: {
+        type: String,
+        required: false
     },
     text: {
         type: String,
         required: true
-    },
-    questionId: {
-        type: String,
-        required: true
     }
-})
+}, {timestamps:true})
 module.exports = mongoose.model("comment", commentSchema)
